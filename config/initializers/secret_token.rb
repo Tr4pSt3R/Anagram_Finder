c9ca0692f2abec80815cd991f9c712fc1f9e97cb
Tr4pSt3R <mightyj@hotmail.co.uk>
2012-05-19 06:27:50 +0100

    Created a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnagramFinder::Application.config.secret_token = '92c95f11a6aecf061115fd46a569e393b655aaf2e62b49573050532896d10080ab8ad03ffdba974ad69ceb7514a52f7b5a6c22282242c804408f533d45e1df8d'
